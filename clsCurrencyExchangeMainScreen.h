f7ab709e033a5234874150e06d0b7e8311f02dd5
Dahou Mohammed <Dahou.mohammed1999@gmail.com>
2026-05-13 16:21:32 +0200

    Currencies List Screen

--- clsCurrencyExchangeMainScreen.h
+++ clsCurrencyExchangeMainScreen.h
@@ -3,6 +3,7 @@
 #include "clsScreen.h"
 #include "clsInputValidate.h"
 #include <iomanip>
+#include "clsCurrenciesListScreen.h"
 
 using namespace std;
 
@@ -31,8 +32,8 @@ private:
 
     static void _ShowCurrenciesListScreen()
     {
-        cout << "\nCurriencies List Screen Will Be Here.\n";
-
+        //  cout << "\nCurriencies List Screen Will Be Here.\n";
+        clsCurrenciesListScreen::ShowCurrenciesListScreen();
     }
 
     static void _ShowFindCurrencyScreen()
